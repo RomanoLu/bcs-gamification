98ff0008c6693b9c88b636019a2b0f09dcb64c03
kuruscufatih <fatihkuruscu@icloud.com>
2022-04-25 14:38:19 +0200

    Navbar Styled
    Navbar finished with styling
    #readyForJS

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,77 @@
 import React from 'react'
 import Logo from '../Pictures/bcs-logo.png'
 import './HeaderStyles.css';
-import {Navbar, Nav, Button, Form, FormControl, Container} from 'react-bootstrap'
- 
+import {FaSearch, FaStar, FaBell} from 'react-icons/fa';
+import {GiLetterBomb} from 'react-icons/gi';
+import {RiTimerLine, RiSettings5Fill} from 'react-icons/ri';
+import {Navbar, Nav, Tabs, Tab, Sonnet} from 'react-bootstrap';
+import {HiQuestionMarkCircle} from 'react-icons/hi';
 
 
 const Header = () => {
   return (
-    <div>
-        <Navbar bg="light" variant="light">
-            <Container>
-            <Nav className="me-auto">
-                <Nav.Link className="me-auto2">Mein Bereich</Nav.Link>
-                <Nav.Link className="me-auto2">PRODUKTE</Nav.Link>
-                <Nav.Link className="me-auto2">TICKETS</Nav.Link>
-                <Nav.Link className="me-auto2">PROFIL</Nav.Link>
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="search">
+          <Navbar>
+            <Navbar.Brand> <img src={Logo} width="30px" height="30px" />{' '} </Navbar.Brand>
+            <Navbar.Toggle className="coloring" />
+            <Navbar.Collapse>
+              <Nav>
+                <Nav.Link href="#blog">Mein Bereich</Nav.Link>
+                <Nav.Link href="#about-us">Tickets</Nav.Link>
+                <Nav.Link href="#contact-us">Profil</Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
+          </Navbar>
+        </div>
+        <div className="items">
+          <div className="item">
+            <FaSearch className="icon" />
+          </div>
+          <div className="item">
+            <FaStar className="icon"/>
+          </div>
+          <div className="item">
+            <GiLetterBomb className="icon" />
+          </div>
+          <div className="item">
+            <FaBell className="icon" />
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <RiTimerLine className="icon" />
+            Starten
+          </div>
+          <div className="item">
+            <HiQuestionMarkCircle className="icon" />
+          </div>
+          <div className="item">
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C4D03AQEoIHMPNhI2BQ/profile-displayphoto-shrink_400_400/0/1593272066605?e=1655942400&v=beta&t=en7EBo-drD3KjfUgnA8OkoKOAzbfmeviHVRyq5DK7Uk"
+              alt=""
+              className="avatar"
+            />
+          </div>
+          <div className="item">
+            <RiSettings5Fill className="icon" />
+          </div>
+        </div>
+      </div>
 
-                <div id="indicator"></div>
-            </Nav>
-            </Container>
-        </Navbar>
 
-
+      <div className='tabs'>
+              <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
+            <Tab eventKey="home" title="Home">
+            </Tab>
+            <Tab eventKey="profile" title="Profile">
+            </Tab>
+            <Tab eventKey="contact" title="Contact" disabled>
+            </Tab>
+          </Tabs>
+      </div>
     </div>
+    
 
   )
 }
