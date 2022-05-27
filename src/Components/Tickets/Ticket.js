4821d5ada3c20cad754192166fcbe3162ad16903
kuruscufatih <fatihkuruscu@icloud.com>
2022-05-27 10:26:18 +0200

    Update Ticket.js

--- src/Components/Tickets/Ticket.js
+++ src/Components/Tickets/Ticket.js
@@ -36,7 +36,7 @@ class Popup extends React.Component {
                     <Form className={TicketCSS.formcontainer}>
                         <div className={TicketCSS.parent}>
                             <div className={TicketCSS.div1}>
-                                <Form.Group className={TicketCSS.mb-3} htmlFor="disabledTextInput">
+                                <Form.Group className={TicketCSS.mb - 3} htmlFor="disabledTextInput">
                                     <Form.Label>Betreff <span style={{ color: 'red' }}>*</span></Form.Label>
                                     <Form.Control 
                                         onChange={(e) => {
@@ -47,170 +47,90 @@ class Popup extends React.Component {
                                     />
                                 </Form.Group>
 
-                <Form.Group className={style.mb - 3}>
-                  <Form.Label htmlFor="disabledTextInput">
-                    Ticketart <span style={{ color: "red" }}>*</span>
-                  </Form.Label>
+                                <Form.Group className={TicketCSS.mb-3}>
+                                    <Form.Label  htmlFor="disabledTextInput">
+                                        Ticketart <span style={{ color: 'red' }}>*</span>
+                                    </Form.Label>
 
-                  <Form.Select
-                    id="disabledSelect"
-                    onChange={(e) => {
-                      this.setState({ art: e.target.value });
-                    }}
-                  >
-                    {/*TODO Entweder zeit in Db ändern oder wir müssen hier system zeit nehmen und dann mit den optionen verrechnen und das ergebnis schreiben*/}
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Fehler
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Anpassung
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Dienstleistung
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Offener Punkt
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Vorschlag
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Anfrage
-                    </option>
-                  </Form.Select>
-                </Form.Group>
-                <Form.Group
-                  className={style.mb - 3}
-                  htmlFor="disabledTextInput"
-                >
-                  <Form.Label>
-                    Bezug <span style={{ color: "red" }}>*</span>
-                  </Form.Label>
-                  <Form.Control
-                    onChange={(e) => {
-                      this.setState({ bezug: e.target.value });
-                    }}
-                    className={style.tfstyle}
-                    type="email"
-                  />
-                </Form.Group>
-                <Form.Group className={style.mb - 3}>
-                  <Form.Label>
-                    Status <span style={{ color: "red" }}>*</span>
-                  </Form.Label>
+                                    <Form.Select id="disabledSelect" onChange={(e) => {
+                                        this.setState({ art: e.target.value })
 
-                  <Form.Select
-                    id="disabledSelect"
-                    onChange={(e) => {
-                      this.setState({ sta: e.target.value });
-                    }}
-                  >
-                    {/*TODO Entweder zeit in Db ändern oder wir müssen hier system zeit nehmen und dann mit den optionen verrechnen und das ergebnis schreiben*/}
-                    <option
-                      style={{ color: "blue", backgroundColor: "#f0f9ff" }}
-                    >
-                      Neu
-                    </option>
-                    <option
-                      style={{ color: "red", backgroundColor: "#f0f9ff" }}
-                    >
-                      Gesichtet
-                    </option>
-                    <option
-                      style={{ color: "purple", backgroundColor: "#f0f9ff" }}
-                    >
-                      Klärung
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Absprache
-                    </option>
-                    <option
-                      style={{ color: "purple", backgroundColor: "#f0f9ff" }}
-                    >
-                      Angeboten
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Aufgenommen
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Eingeplant
-                    </option>
-                    <option
-                      style={{ color: "red", backgroundColor: "#f0f9ff" }}
-                    >
-                      Bearbeitung
-                    </option>
-                    <option
-                      style={{ color: "green", backgroundColor: "#f0f9ff" }}
-                    >
-                      Abnahme
-                    </option>
-                    <option
-                      style={{ color: "green", backgroundColor: "#f0f9ff" }}
-                    >
-                      Geschlossen
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Unbeantwortet
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      (Importiert)
-                    </option>
-                  </Form.Select>
-                </Form.Group>
-                <Form.Group className={style.mb - 3}>
-                  <Form.Label>
-                    Priorität (intern) <span style={{ color: "red" }}>*</span>
-                  </Form.Label>
+                                        }
+                                        }>
+                                        {/*TODO Entweder zeit in Db ändern oder wir müssen hier system zeit nehmen und dann mit den optionen verrechnen und das ergebnis schreiben*/}
+                                        <option style = {{ backgroundColor: '#f0f9ff' }} >Fehler</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Anpassung</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Dienstleistung</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Offener Punkt</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Vorschlag</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Anfrage</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className={TicketCSS.mb-3} htmlFor="disabledTextInput">
+                                    <Form.Label>Bezug <span style={{ color: 'red' }}>*</span></Form.Label>
+                                    <Form.Control 
+                                        onChange={(e) => {
+                                            this.setState({ bezug: e.target.value })
+                                        }} 
+                                        className={TicketCSS.tfstyle}
+                                        type="email" 
+                                    />
+                                </Form.Group>
+                                <Form.Group className={TicketCSS.mb-3}>
+                                    <Form.Label>Status <span style={{ color: 'red' }}>*</span></Form.Label>
 
-                  <Form.Select
-                    id="disabledSelect"
-                    onChange={(e) => {
-                      this.setState({ prio: e.target.value });
-                    }}
-                  >
-                    {/*TODO Entweder zeit in Db ändern oder wir müssen hier system zeit nehmen und dann mit den optionen verrechnen und das ergebnis schreiben*/}
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Maximal
-                    </option>
-                    <option
-                      style={{ color: "#FF0000", backgroundColor: "#f0f9ff" }}
-                    >
-                      Sehr Hoch
-                    </option>
-                    <option
-                      style={{ color: "#AA0000", backgroundColor: "#f0f9ff" }}
-                    >
-                      Hoch
-                    </option>
-                    <option style={{ backgroundColor: "#f0f9ff" }}>
-                      Normal
-                    </option>
-                    <option
-                      style={{ color: "#c7c7c7", backgroundColor: "#f0f9ff" }}
-                    >
-                      Niedrig
-                    </option>
-                  </Form.Select>
-                </Form.Group>
-              </div>
-              <div className="div2">
-                <Form.Group className={style.mb - 3} controlId="formBasicEmail">
-                  <Form.Label>
-                    Beschreibung <span style={{ color: "red" }}>*</span>
-                  </Form.Label>
-                  <Form.Control className="tfstyle2" type="email" />
-                </Form.Group>
-              </div>
+                                    <Form.Select id="disabledSelect" onChange={(e) => {
+                                        this.setState({ sta: e.target.value })
+                                        }}
+                                        >
+                                        {/*TODO Entweder zeit in Db ändern oder wir müssen hier system zeit nehmen und dann mit den optionen verrechnen und das ergebnis schreiben*/}
+                                        <option style = {{ color:'blue', backgroundColor: '#f0f9ff' }} >Neu</option>
+                                        <option style = {{  color: 'red', backgroundColor: '#f0f9ff' }}>Gesichtet</option>
+                                        <option style = {{  color: 'purple',backgroundColor: '#f0f9ff' }}>Klärung</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Absprache</option>
+                                        <option style = {{  color: 'purple',backgroundColor: '#f0f9ff' }}>Angeboten</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Aufgenommen</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Eingeplant</option>
+                                        <option style = {{  color: 'red', backgroundColor: '#f0f9ff' }}>Bearbeitung</option>
+                                        <option style = {{ color: 'green',backgroundColor: '#f0f9ff' }}>Abnahme</option>
+                                        <option style = {{ color: 'green',backgroundColor: '#f0f9ff' }}>Geschlossen</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Unbeantwortet</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>(Importiert)</option>
+                                    </Form.Select>
+                                </Form.Group>
+                                <Form.Group className={TicketCSS.mb-3}>                                
+                                    <Form.Label>Priorität (intern) <span style={{ color: 'red' }}>*</span></Form.Label>                                
+                                    
+                                    <Form.Select id="disabledSelect" onChange={(e) => {
+                                        this.setState({ prio: e.target.value })
+
+                                        }
+                                        }>
+                                        {/*TODO Entweder zeit in Db ändern oder wir müssen hier system zeit nehmen und dann mit den optionen verrechnen und das ergebnis schreiben*/}
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Maximal</option>
+                                        <option style = {{  color: '#FF0000',backgroundColor: '#f0f9ff' }}>Sehr Hoch</option>
+                                        <option style = {{ color: '#AA0000',backgroundColor: '#f0f9ff' }}>Hoch</option>
+                                        <option style = {{ backgroundColor: '#f0f9ff' }}>Normal</option>
+                                        <option style = {{ color: '#c7c7c7', backgroundColor: '#f0f9ff' }}>Niedrig</option>
+
+                                    </Form.Select>                                    
+                                </Form.Group>
+                            </div>
+                            <div className='div2'>
+                                <Form.Group className={TicketCSS.mb-3} controlId="formBasicEmail">
+                                    <Form.Label>Beschreibung <span style={{ color: 'red' }}>*</span></Form.Label>
+                                    <Form.Control className='tfstyle2' type="email" />
+                                </Form.Group>
+                            </div>
+                        </div>
+                        <Button variant="primary" onClick={submit}>
+                            Submit
+                        </Button>
+                    </Form>
+                </div>
             </div>
-            <Button variant="primary" onClick={submit}>
-              Submit
-            </Button>
-          </Form>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 class Ticket extends Component {
@@ -244,33 +164,31 @@ class Ticket extends Component {
             <div className={TicketCSS.container}>
                 <h2>Meine Tickets: Alle meine Tickets</h2>
 
-        <div className={style.buttons}>
-          <Button variant="light" onClick={this.togglePopup.bind(this)}>
-            Neu
-          </Button>{" "}
-          <Button variant="primary">Bearbeiten</Button>{" "}
-        </div>
-        <Dropdown>
-          <Dropdown.Toggle id="1" size="sm" variant="secondary">
-            Zeitraum
-          </Dropdown.Toggle>
-          <Dropdown.Menu variant="dark1"></Dropdown.Menu>
-          <Dropdown.Toggle id="2" size="sm" variant="secondary">
-            Eingang
-          </Dropdown.Toggle>
+                <div className={TicketCSS.buttons}>
+                    <Button variant="light" onClick={this.togglePopup.bind(this)}>Neu</Button> <Button variant="primary">Bearbeiten</Button>{' '}
+                </div>
+                <Dropdown>
+                    <Dropdown.Toggle id="1" size="sm" variant="secondary">
+                        Zeitraum
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu variant="dark1">
+                    </Dropdown.Menu>
+                    <Dropdown.Toggle id="2" size="sm" variant="secondary">
+                        Eingang
+                    </Dropdown.Toggle>
 
-          <Dropdown.Menu variant="dark1">
-            <Dropdown.Item href="#/action-1" active>
-              Wunschtermin
-            </Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Termin</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Eingefügt</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Item href="#/action-4">Aktualisiert am</Dropdown.Item>
-          </Dropdown.Menu>
-          <Dropdown.Toggle id="3" size="sm" variant="secondary">
-            Gesamt
-          </Dropdown.Toggle>
+                    <Dropdown.Menu variant="dark1">
+                        <Dropdown.Item href="#/action-1" active>
+                            Wunschtermin
+                        </Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Termin</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Eingefügt</Dropdown.Item>
+                        <Dropdown.Divider />
+                        <Dropdown.Item href="#/action-4">Aktualisiert am</Dropdown.Item>
+                    </Dropdown.Menu>
+                    <Dropdown.Toggle id="3" size="sm" variant="secondary">
+                        Gesamt
+                    </Dropdown.Toggle>
 
                     <Dropdown.Menu variant="dark2">
                         <Dropdown.Item href="#/action-1" active>
@@ -285,12 +203,8 @@ class Ticket extends Component {
 
                 </Dropdown>
 
-
-
-
-
                 <div className={TicketCSS.cpntainer2}>
-                    <Table responsive="sm">
+                    <Table responsive="sm" >
                         <tbody>
                             <tr className={TicketCSS.secondhead}>
                                 <td>ID</td>
@@ -326,7 +240,8 @@ class Ticket extends Component {
             </div>
 
         )
-  }
+    }
+
 }
 
-export default Ticket;
+export default Ticket
